8971076f078498114c179bb9a26429d23fbc91f9
unknown <BT_3W2_17@bktech.local>
2023-03-23 08:20:25 -0400

    Initial commit

---
+++ src/Tablet.java
@@ -0,0 +1,11 @@
+public class Tablet extends Computer{
+    private int screenSize; // in inches
+    private int memory; // in gigabytes (GB)
+    private boolean stylusIncluded;
+
+    public Tablet(int screenSize, int memory, boolean stylusIncluded) {
+        super(screenSize, memory);
+        this.stylusIncluded = stylusIncluded;
+    }
+
+}
